24eceedf0af2b2047ce8b348cfb9f0567422dbcf
Ismail alla <79142519+IsmailALLA2@users.noreply.github.com>
2025-05-09 15:04:35 +0100

    add agency , dropdown

--- src/main/java/com/example/demo/repository/AgenceRepository.java
+++ src/main/java/com/example/demo/repository/AgenceRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Agence;
-import com.example.demo.entity.Step1Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface AgenceRepository extends JpaRepository<Agence, Long> {
+    List<Agence> findByCountry(String country);
+    Optional<Agence> findByName(String name);
+    List<Agence> findByNameContainingIgnoreCase(String name);
 }
